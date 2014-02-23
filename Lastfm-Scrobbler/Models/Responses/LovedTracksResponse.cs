6b2350dac3cdf877f2a2e930dbdf54010aa600f6
MrSmoke <mrsmoke@nizzebix.com>
2014-02-23 20:37:00 +1100

    Added missing files
    Missed committing models
    Also ignore packages folder

---
+++ Lastfm-Scrobbler/Models/Responses/LovedTracksResponse.cs
@@ -0,0 +1,25 @@
+﻿namespace LastfmScrobbler.Models.Responses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class LovedTracksResponse : BaseResponse
+    {
+        [DataMember(Name="lovedtracks")]
+        public LovedTracks LovedTracks { get; set; }
+
+        public bool HasLovedTracks()
+        {
+            return LovedTracks != null && LovedTracks.Tracks != null && LovedTracks.Tracks.Count > 0;
+        }
+    }
+
+    [DataContract]
+    public class LovedTracks
+    {
+        [DataMember(Name = "track")]
+        public List<LastfmLovedTrack> Tracks { get; set; }
+    }
+}
---
+++ Lastfm-Scrobbler/Models/Tracks.cs
@@ -0,0 +1,30 @@
+﻿namespace LastfmScrobbler.Models
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class BaseLastfmTrack
+    {
+        [DataMember(Name="artist")]
+        public LastfmArtist Artist { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+    }
+
+    [DataContract]
+    public class LastfmArtist
+    {
+        [DataMember(Name="name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "mbid")]
+        public string MusicBrainzId { get; set; }
+    }
+
+    public class LastfmLovedTrack : BaseLastfmTrack
+    {
+
+    }
+}
